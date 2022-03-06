6fa7f692f5145d66d4092348d977e3f08b39938e
Aryan Bhatia <aryan.bhatia@droneshield.com>
2022-03-06 22:20:57 +1100

    Packages

---
+++ 03_packages/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/aryanbh11/go_crash_course/03_packages/strutil"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(strutil.Reverse("hello"))
+}
